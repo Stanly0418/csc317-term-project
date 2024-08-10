33d8143fbc1766b959746751b10a7fece6fea9d3
Stanly0418 <wf1003111614@gmail.com>
2024-08-10 06:38:56 -0700

    Initial commit

--- app.js
+++ app.js
@@ -1,88 +1,126 @@
 const createError = require("http-errors");
 const express = require("express");
-const favicon = require('serve-favicon');
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const handlebars = require("express-handlebars");
+const favicon = require('serve-favicon');
+
+// Import route modules
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
 
+// Set up Handlebars view engine
 app.engine(
   "hbs",
   handlebars({
-    layoutsDir: path.join(__dirname, "views/layouts"), //where to look for layouts
-    partialsDir: path.join(__dirname, "views/partials"), // where to look for partials
-    extname: ".hbs", //expected file extension for handlebars files
-    defaultLayout: "layout", //default layout for app, general template for all pages in app
-    helpers: {}, //adding new helpers to handlebars for extra functionality
+    layoutsDir: path.join(__dirname, "views/layouts"), // Layouts directory
+    partialsDir: path.join(__dirname, "views/partials"), // Partials directory
+    extname: ".hbs", // File extension
+    defaultLayout: "layout", // Default layout
+    helpers: {}, // You can add custom Handlebars helpers here
   })
 );
 
-// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Set favicon
 app.use(favicon(__dirname + '/public/favicon.ico'));
+
+// Static files directory
 app.use("/public", express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter); // route middleware from ./routes/index.js
-app.use("/users", usersRouter); // route middleware from ./routes/users.js
+// Set up route middleware
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
+// MySQL database configuration
+const mysql = require('mysql2');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
 
-const users = [];
-
-// Simple user database
-app.post('/register', (req, res) => {
-  const { username, password, email } = req.body;
-  users.push({ username, password, email });
-  res.send('User registered successfully');
+// Database connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '0418',
+  database: 'csc317_term_project'
 });
 
-// Login route
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  const user = users.find(u => u.username === username && u.password === password);
-  if (user) {
-    req.session.user = user;
-    res.send('Login successful');
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
   } else {
-    res.send('Invalid credentials');
+    console.log('Connected to MySQL database.');
   }
 });
 
-// Logout route
+// Session store setup
+const sessionStore = new MySQLStore({}, connection);
+
+app.use(session({
+  key: 'session_cookie_name',
+  secret: 'session_cookie_secret',
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false
+}));
+
+// User registration
+app.post('/register', (req, res) => {
+  const { username, password, email } = req.body;
+  const query = 'INSERT INTO users (username, password, email) VALUES (?, ?, ?)';
+  connection.query(query, [username, password, email], (err, results) => {
+    if (err) {
+      console.error('Error registering user:', err);
+      res.status(500).send('Internal server error');
+    } else {
+      res.send('User registered successfully');
+    }
+  });
+});
+
+// User login
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+  connection.query(query, [username, password], (err, results) => {
+    if (err) {
+      console.error('Error during login:', err);
+      res.status(500).send('Internal server error');
+    } else if (results.length > 0) {
+      req.session.user = results[0];
+      res.send('Login successful');
+    } else {
+      res.send('Invalid credentials');
+    }
+  });
+});
+
+// User logout
 app.get('/logout', (req, res) => {
   req.session.destroy();
   res.send('Logged out successfully');
 });
 
-/**
- * Catch all route, if we get to here then the 
- * resource requested could not be found.
- */
-app.use((req,res,next) => {
+// Catch all unhandled requests
+app.use((req, res, next) => {
   next(createError(404, `The route ${req.method} : ${req.url} does not exist.`));
-})
-  
+});
 
-/**
- * Error Handler, used to render the error html file
- * with relevant error information.
- */
+// Error handler
 app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = err;
   console.log(err);
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: 'localhost',
+    user: 'root',
+    password: '0418',
+    database: 'csc317_term_project',
+  };
+  
